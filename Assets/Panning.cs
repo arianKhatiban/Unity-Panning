7e24281a4abad86d73c82ed25778ee1dd96c93f1
arianKhatiban <arianf008@gmail.com>
2021-10-16 01:02:56 +0330

    Initial commit

---
+++ Assets/Panning.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Panning : MonoBehaviour
+{
+    [SerializeField][Range(0.01f,2)] private float speed;
+
+    private Vector3 panStart;
+    private Camera cam;
+
+    private void Start() {
+        cam = GetComponent<Camera>();
+    }
+
+    private void Update() {
+        if(Input.GetMouseButtonDown(0)){
+            panStart = cam.ScreenToWorldPoint(Input.mousePosition);
+        }
+
+        if(Input.GetMouseButton(0)){
+            Vector3 direction = panStart - cam.ScreenToWorldPoint(Input.mousePosition);
+            cam.transform.position += direction * speed;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Unity-Panning
+ You can pan (Camera movement) around  the screen for Windows or mobile devices
